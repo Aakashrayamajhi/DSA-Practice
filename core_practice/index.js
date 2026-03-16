8db62885f9acebcc531db04c7c5aa97680ba9ef5
Aakashrayamajhi <aakashrayamajhi03@gmail.com>
2026-03-16 11:18:41 +0545

    type coercion

--- core_practice/index.js
+++ core_practice/index.js
@@ -1,3 +1,14 @@
-let age = prompt("whats you age bruda:")
-console.log(typeof age) //-> string , reason is so
-console.log(typeof Number(age)) //->number why? bcz - Number(age) tries to convert age into number if string -"dfj" cames in prompt then the function return NaN - NOT A NUMBER then typeof return number bcz nan is related to number anything related to number will be return as number by typeof. 
+// let age = prompt("whats you age bruda:")
+// console.log(typeof age) //-> string , reason is so
+// console.log(typeof Number(age)) //->number why? bcz - Number(age) tries to convert age into number if string -"dfj" cames in prompt then the function return NaN - NOT A NUMBER then typeof return number bcz nan is related to number anything related to number will be return as number by typeof. 
+
+// ======= TYPE CORECOIN==========
+
+let a = 10
+let b = "1"
+console.log(a+b) //-> 101 bcz of concatination , but
+//but in other operator have only work to do that is mathmetical calcualtion , so they converted the sting that is 1 into a number and cal math logic.
+console.log(a-b) //->9
+console.log(a*b) //->10 .. and so on
+let c = "sf"
+console.log(b-c)
