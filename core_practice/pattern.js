9e1ec0c5126d5901fb4f8ef527517984f05a9b66
Aakashrayamajhi <aakashrayamajhi03@gmail.com>
2026-03-21 21:44:46 +0545

    some basic to advance pattern programming

--- core_practice/pattern.js
+++ core_practice/pattern.js
@@ -1,10 +1,115 @@
-let prompt = require('prompt-sync')()
-let a = Number(prompt("enter a a number"))
+// let prompt = require('prompt-sync')()
+// let a = Number(prompt("enter a a number"))
 
-for(let i = 1 ; i <= a ; i++){
-for(let j = 1 ; j <=a; j++){
-process.stdout.write('# ')
-}
-console.log()
+// for(let i = 1 ; i <= a ; i++){
+// for(let j = 1 ; j <=a; j++){
+// process.stdout.write('# ')
+// }
+// console.log()
 
-}
+// }
+
+
+// let prompt = require('prompt-sync')()
+// let n = Number(prompt("enter a  number"))
+
+// for(let i = 1 ; i <= n ; i++){
+// for(let j = 1 ; j <= n ; j++){
+// process.stdout.write(' ')
+// for(let k = 1; k<=n ; k++){
+//    process.stdout.write('# ')
+// }
+// }
+// console.log()
+
+// }
+
+// //  # # # # #  # # # # #  # # # # #  # # # # #  # # # # # 
+// //  # # # # #  # # # # #  # # # # #  # # # # #  # # # # #
+// //  # # # # #  # # # # #  # # # # #  # # # # #  # # # # #
+// //  # # # # #  # # # # #  # # # # #  # # # # #  # # # # #
+// //  # # # # #  # # # # #  # # # # #  # # # # #  # # # # #
+
+
+// for (let i = 1; i <= 15; i++) {
+//     console.log("|")
+//     if (i === 5) {
+//         for (let j = 1; j <= 1; j++) {
+//             {
+//                 for (let k = 1; k <= 5; k++) {
+//                     process.stdout.write(" - ")
+//                 }
+//                 console.log("")
+//             }
+//         }
+//     }
+//     if (i === 10) {
+//         for (let j = 1; j <= 1; j++) {
+//             {
+//                 for (let k = 1; k <= 5; k++) {
+//                     process.stdout.write(" - ")
+//                 }
+//                 console.log("")
+//             }
+//         }
+//     }
+
+
+// }
+
+// // |
+// // |
+// // |
+// // |
+// // |
+// //  -  -  -  -  -
+// // |
+// // |
+// // |
+// // |
+// // |
+// //  -  -  -  -  -
+// // |
+// // |
+// // |
+// // |
+// // |
+
+
+
+// for (let i = 1; i <= 5; i++) {
+//     let row = "";
+
+//     for (let j = 1; j <= i; j++) {
+//         row += "*";
+//     }
+
+//     console.log(row);
+// }
+
+// for (let i = 1; i <= 5; i++) {
+//     let row = "";
+
+//     for (let j = 1; j <= i; j++) {
+//         row += "*";
+//     }
+
+//     console.log(row);
+// }
+
+// for( let i =1; i<= 2; i++){
+//     console.log("*")
+// }
+
+// // *
+// // **
+// // ***
+// // ****
+// // *****
+// // *
+// // **
+// // ***
+// // ****
+// // *****
+// // *
+// // *
