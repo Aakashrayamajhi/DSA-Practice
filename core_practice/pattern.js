a8a826520db44d3e1a0e0a6aa3a2fa5e79a817bd
Aakashrayamajhi <aakashrayamajhi03@gmail.com>
2026-03-21 20:49:51 +0545

    pattern programming

---
+++ core_practice/pattern.js
@@ -0,0 +1,10 @@
+let prompt = require('prompt-sync')()
+let a = Number(prompt("enter a a number"))
+
+for(let i = 1 ; i <= a ; i++){
+for(let j = 1 ; j <=a; j++){
+process.stdout.write('# ')
+}
+console.log()
+
+}
