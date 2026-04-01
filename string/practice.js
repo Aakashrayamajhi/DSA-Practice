cb67575157c0bdf894999fe5e73f63f137ab3ae4
Aakashrayamajhi <aakashrayamajhi03@gmail.com>
2026-04-01 20:37:36 +0545

    string

---
+++ string/practice.js
@@ -0,0 +1,37 @@
+// let s = "aakash"
+// for(let i = 0; i<s.length ; i++){
+//     console.log(s.charAt(i))
+// }
+
+// let s = "madam"
+// for(let i = s.length-1; i>=0; i--){
+//     console.log(s.charAt(i))
+// }
+
+
+// let s = "madam"
+// let j =1
+// let isPalindrome = true
+// for(let i = 0; i<s.length/2 ; i++){
+//    if (s[i] == s[s.length - j]){
+
+//    }else{
+//     isPalindrome = false
+//     break;
+//    }
+//        j++
+// }
+
+// if(isPalindrome){
+//     console.log("string is palindrome")
+// }else{
+//     console.log("string is not palindrome")
+// }
+
+// let s = "hhhhii"
+// let rev = ""
+
+// for(let i = s.length-1 ; i >=0 ; i--){
+//     rev = rev.concat(s[i])
+// }
+// console.log(rev)
