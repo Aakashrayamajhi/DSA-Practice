66a7c348e9785076f817c64a0c1821d42f4b59e4
Aakashrayamajhi <aakashrayamajhi03@gmail.com>
2026-06-20 11:50:59 +0545

    using set data structure

---
+++ hashing/set.js
@@ -0,0 +1,13 @@
+let arr = [1, 2, 3, 4, 6, 5, 4, 3, 2, 1, 6]
+
+let set = new Set()
+
+for (let i = 0; i < arr.length; i++) {
+    if (set.has(arr[i])) {
+        set.delete(arr[i])
+    } else {
+        set.add(arr[i])
+    }
+}
+
+console.log(set)
