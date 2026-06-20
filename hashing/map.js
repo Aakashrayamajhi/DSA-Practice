3c74aff0d6f1791b4f7dedba17369fe19472f7b2
Aakashrayamajhi <aakashrayamajhi03@gmail.com>
2026-06-20 12:35:05 +0545

    playing with map

---
+++ hashing/map.js
@@ -0,0 +1,44 @@
+let map = new Map()
+
+map.set("name", "Aakash Jung Rayamajhi")
+map.set("age" , 23)
+map.set("address" , "suddhodhan-7 manmateria")
+
+for(let [key , value] of map){
+    console.log(key + "==>" + value)
+}
+
+console.log("lets print only keys")
+
+for(let key of map.keys()){
+    console.log(key)
+}
+
+console.log("lets print only values")
+
+for(let values of map.values()){
+    console.log(values)
+}
+
+console.log("lets to some extra creative stuff")
+
+for(let key of map){
+    console.log(key)
+}
+
+console.log("lets check its size")
+console.log(map.size)
+
+console.log("lets check 23 is availabe in vlaue or not")
+
+function findValue(map) {
+    for (let value of map.values()) {
+        if (value === 23) {
+            return true;
+        }
+    }
+    return false
+}
+console.log(findValue(map))
+
+console.log(map)
--- hashing/set.js
+++ hashing/set.js
@@ -1,13 +1,17 @@
 let arr = [1, 2, 3, 4, 6, 5, 4, 3, 2, 1, 6]
 
-let set = new Set()
+let set = new Set(arr)
 
-for (let i = 0; i < arr.length; i++) {
-    if (set.has(arr[i])) {
-        set.delete(arr[i])
-    } else {
-        set.add(arr[i])
-    }
-}
+// for (let i = 0; i < arr.length; i++) {
+//     if (set.has(arr[i])) {
+//         set.delete(arr[i])
+//     } else {
+//         set.add(arr[i])
+//     }
+// }
+
+// for( let a of arr){
+//   set.add(a)
+// }
 
 console.log(set)
