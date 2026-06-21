23aba5f581311092289ee897f091c838c38a3695
Aakashrayamajhi <aakashrayamajhi03@gmail.com>
2026-06-21 08:35:34 +0545

    started day with full motivation continuing hashing

--- hashing/leetcode.js
+++ hashing/leetcode.js
@@ -1,30 +1,54 @@
-var getLeastFrequentDigit = function(n) {
-    let str = String(n)
-    let map = new Map()
+// var getLeastFrequentDigit = function(n) {
+//     let str = String(n)
+//     let map = new Map()
     
-    for(let i = 0 ; i < str.length ; i++){
-        if(map.has(str[i])){
-            map.set(str[i] , map.get(str[i]) + 1)
-        }else{
-            map.set(str[i] , 1 )
-        }  
-    }
+//     for(let i = 0 ; i < str.length ; i++){
+//         if(map.has(str[i])){
+//             map.set(str[i] , map.get(str[i]) + 1)
+//         }else{
+//             map.set(str[i] , 1 )
+//         }  
+//     }
 
-let count = Infinity
-let number = 9
+// let count = Infinity
+// let number = 9
 
-for( let [key , value] of map ){
-   key = Number(key)
-    if(count > value || (count ===  value && key < number)){
-        count = value  //1
-        number = key
-    }
+// for( let [key , value] of map ){
+//    key = Number(key)
+//     if(count > value || (count ===  value && key < number)){
+//         count = value  //1
+//         number = key
+//     }
   
+// }
+//   console.log(number)
+
+
+
+// };
+
+// getLeastFrequentDigit(11553322)
+
+
+// --------=== sort the people===---------
+
+function sortpeople(names , heights){
+ let map = new Map()
+ let newheights = [...heights].sort((a,b)=>b-a)
+ console.log(newheights)
+
+for(let i =0 ; i< heights.length ;  i++){
+    map.set(heights[i] , names[i])
 }
-  console.log(number)
 
+let result = []
 
+for(let i = 0 ; i < newheights.length ; i++){
+   result.push(map.get(newheights[i]))
+}
 
-};
+ console.log(result)
 
-getLeastFrequentDigit(11553322)
+}
+
+sortpeople(["a" , "b" , "c"] , [1 , 2 , 3])
