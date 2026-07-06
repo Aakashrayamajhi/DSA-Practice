6b482ee854a880eb56e5b251b17d285618274ac0
Aakashrayamajhi <aakashrayamajhi03@gmail.com>
2026-07-06 16:15:28 +0545

    some of the trial

---
+++ leetcode/125.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+
+  let str = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "")
+
+  let i = 0
+  let j = str.length - 1
+
+  while(i < j){
+    if(str[i] !== str[j]) return false
+
+    i++
+    j--
+  } return true
+};
